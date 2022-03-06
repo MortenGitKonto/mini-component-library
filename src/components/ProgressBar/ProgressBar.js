e6e080ab0db5dc68a18f84907b8ede32a324e69b
Morten <morten.jogi@gmail.com>
2022-03-06 15:48:44 +0100

    course changes

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,10 +3,80 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from '../VisuallyHidden/VisuallyHidden';
+
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    borderRadius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    borderRadius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    borderRadius: 8,
+  },
+}
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+  const styles = STYLES[size]
+
+  if (!styles) throw new Error(`Unknwon size passed in`)
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.borderRadius + "px"
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper >
+        <Bar style={{
+          "--width": value + "%",
+          "--height": styles.height + "px"
+
+        }} />
+      </BarWrapper>
+    </Wrapper>
+  )
 };
+
+
+const Wrapper = styled.div`
+background-color:${COLORS.transparentGray15};
+box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+border-radius:var(--radius);
+// Trim off corners when progress bar is near full
+overflow:hidden;
+padding:var(--padding)
+`
+
+const BarWrapper = styled.div`
+border-radius:4px;
+overflow:hidden;
+`
+
+
+const Bar = styled.div`
+width:var(--width);
+height:var(--height);
+background-color:${COLORS.primary};
+border-radius:4px 0 0 4px;
+
+`
+
+
+
 
 export default ProgressBar;
